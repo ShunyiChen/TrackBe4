3802e4c14517b8298c8f69059e5ffe6f5b0b8557
Shunyi Chen <526637060@qq.com>
2019-08-20 00:28:55 +0800

    Add support for Spring data jpa

--- src/main/java/com/maxtree/automotive/dashboard/TB4Application.java
+++ src/main/java/com/maxtree/automotive/dashboard/TB4Application.java
@@ -1,5 +1,7 @@
 package com.maxtree.automotive.dashboard;
 
+import com.maxtree.automotive.dashboard.persistence.entity.Customer;
+import com.maxtree.automotive.dashboard.persistence.repository.CustomerRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.maxtree.automotive.dashboard.service.LoggingService;
@@ -55,7 +58,48 @@ public class TB4Application extends SpringBootServletInitializer implements Comm
 //        	dbbuilder.build(jdbcTemplate.getDataSource().getConnection());
 //    	}
     }
-    
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            repository.findById(1L)
+                    .ifPresent(customer -> {
+                        log.info("Customer found with findById(1L):");
+                        log.info("--------------------------------");
+                        log.info(customer.toString());
+                        log.info("");
+                    });
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+            // for (Customer bauer : repository.findByLastName("Bauer")) {
+            // 	log.info(bauer.toString());
+            // }
+            log.info("");
+        };
+    }
+
+
     public static final String PERMISSION_DENIED_MESSAGE = "没有权限。";
     public static final String NAME = "TrackBe4";
 	public static final String VERSION = "1.0 beta";
